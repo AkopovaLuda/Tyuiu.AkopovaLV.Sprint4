134bac7d26e4004d18d52a65a6342620b75babb4
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-12-06 05:07:43 -1200

    Task1

---
+++ Tyuiu.AkopovaLV.Sprint4.Task1.V17.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AkopovaLV.Sprint4.Task1.V17.Lib;
+namespace Tyuiu.AkopovaLV.Sprint4.Task1.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 1, 2, 3 };
+            int res = ds.Calculate(array);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AkopovaLV.Sprint4.Task1.V17/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.AkopovaLV.Sprint4.Task1.V17.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Акопова Л. В. | АСОиУб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы (ввод с клавиатуры)                            *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #17                                                             *");
+        Console.WriteLine("* Выполнила: Акопова Людмила Владимировна | АСОиУб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 14 элементов заполненный         *");
+        Console.WriteLine("* значениями с клавиатуры в диапазоне от 1 до 7 подсчитать сумму четных   *");
+        Console.WriteLine("* элементов массива. С клавиатуры: 1,7,6,3,2,5,7,6,7,6,5,4,7,6            *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int[] array = new int[3];
+        for (int i = 0; i <= array.Length - 1; i++)
+        {
+            Console.WriteLine("Вбейте элемент " + (i + 1) + " из " + array.Length);
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(array));
+    }
+}
