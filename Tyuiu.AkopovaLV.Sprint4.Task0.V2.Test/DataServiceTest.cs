2b7a72441c9556ec589319ce06f95bac86ceaf3f
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-12-06 04:30:08 -1200

    Add project files.

---
+++ Tyuiu.AkopovaLV.Sprint4.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AkopovaLV.Sprint4.Task0.V2.Lib;
+namespace Tyuiu.AkopovaLV.Sprint4.Task0.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
+            int res = ds.GetSumOddArrEl(numsArray);
+            int numsWaitArray = 44;
+            Assert.AreEqual(numsWaitArray, res);
+
+        }
+    }
+}
---
+++ Tyuiu.AkopovaLV.Sprint4.Task0.V2/Program.cs
@@ -0,0 +1,35 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+using Tyuiu.AkopovaLV.Sprint4.Task0.V2.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнила: Акопова Л. В. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнила: Акопова Людмила Владимировна | АСОиУб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Подсчитать сумму нечётных элементов массива.                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int[] numsArray = { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
+        int s;
+        Console.WriteLine("Исходный массив:");
+        for (int i = 0; i <= numsArray.Length - 1; i++)
+        {
+            Console.WriteLine(numsArray[i]);
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        s = ds.GetSumOddArrEl(numsArray);
+        Console.WriteLine(s);
+        Console.ReadKey();
+    }
+}
