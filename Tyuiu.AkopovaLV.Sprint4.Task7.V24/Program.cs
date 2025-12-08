37c0aabfa0beea1b07841f864a6d483ebfb0409b
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-12-08 06:47:46 -1200

    Task7

---
+++ Tyuiu.AkopovaLV.Sprint4.Task7.V24/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.AkopovaLV.Sprint4.Task7.V24.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Акопова Л. В. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы (ввод c клавиатуры)                             *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #24                                                             *");
+        Console.WriteLine("* Выполнила: Акопова Людмила Владимировна | АСОиУб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дана строка из одноразрядных цифр \"651827384219648\".                  *");
+        Console.WriteLine("* Преобразуйте ее в матрицу 5 на 3 и подсчитайте произведение четных      *");
+        Console.WriteLine("* чисел.                                                                  *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int str = 5;
+        int stb = 3;
+        int[,] a = new int[str, stb];
+        string s = "651827384219648";
+        int index = 0;
+
+        Console.WriteLine("\nМассив: ");
+
+        for (int i = 0; i < str; i++)
+        {
+            for (int j = 0; j < stb; j++)
+            {
+                Console.Write($"{s[index]} \t");
+                index++;
+            }
+            Console.WriteLine();
+
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int res = ds.Calculate(str, stb, s);
+
+        Console.WriteLine(res);
+
+    }
+}
